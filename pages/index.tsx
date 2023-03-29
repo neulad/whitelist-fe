c80763c0542daa1a11563e0bc164cb2e9c4d52ef
Uladzimir Kireyeu <uladzimirkireyeu@proton.me>
2023-03-29 19:27:57 +0200

    Change the network to speolia

--- pages/index.tsx
+++ pages/index.tsx
@@ -40,9 +40,9 @@ export default function Home() {
 
     // If user is not connected to the Goerli network, let them know and throw an error
     const { chainId } = await web3Provider.getNetwork();
-    if (chainId !== 5) {
-      window.alert("Change the network to Goerli");
-      throw new Error("Change network to Goerli");
+    if (chainId !== 11155111) {
+      window.alert("Change the network to Sepolia");
+      throw new Error("Change network to Sepolia");
     }
 
     if (needSigner) {
